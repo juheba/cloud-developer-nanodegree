5d0fbaa4fe9157a79ce7588403f79c0196c52893
Julia Hebauf <julia.hebauf@lyth.de>
2021-11-02 10:23:02 +0100

    feat: Erweitert den Service um die Ausgabe einer Travis Environment Variable

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const greeter = process.env.GREETER;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`Microservices rock! Greetings from ${greeter}`);
     await sleep(5000);
   }
 }
