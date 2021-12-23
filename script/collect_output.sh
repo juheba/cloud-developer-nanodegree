8b914b050238b96a8c7fae20fc1f97fd0f840983
Julia Hebauf <julia.hebauf@lyth.de>
2021-12-23 19:28:38 +0100

    adds screenshots and logs for review

---
+++ script/collect_output.sh
@@ -0,0 +1,19 @@
+kubectl get pods > screenshots/kubectl_get_pods_output.txt
+
+kubectl describe services > screenshots/kubectl_describe_services_output.txt
+
+kubectl describe hpa > screenshots/kubectl_describe_hpa_output.txt
+
+kubectl describe deployment > screenshots/kubectl_describe_deployment_output.txt
+
+kubectl get service > screenshots/kubectl_get_service.txt
+
+kubectl get hpa > screenshots/kubectl_get_hpa.txt
+
+kubectl get secret > screenshots/kubectl_get_secret.txt
+
+kubectl get configmap > screenshots/kubectl_get_configmap.txt
+
+kubectl logs $(kubectl get pods -l app=udagram-api-feed --output jsonpath='{.items[0].metadata.name}') > screenshots/kubectl_logs_udagram-api-feed.txt
+
+kubectl logs $(kubectl get pods -l app=udagram-api-user --output jsonpath='{.items[0].metadata.name}') > screenshots/kubectl_logs_udagram-user-feed.txt
