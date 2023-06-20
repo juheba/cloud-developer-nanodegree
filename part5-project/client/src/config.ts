fef9eb60b747dfa4eb6e4faa03fd5b0d5891b638
Julia Hebauf <julia.hebauf@lyth.de>
2023-06-20 09:13:13 +0200

    fix aws region in client project

--- part5-project/client/src/config.ts
+++ part5-project/client/src/config.ts
@@ -1,6 +1,7 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'ahb9gjym47'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const region = 'eu-central-1'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
   // Create an Auth0 application and copy values from it into this map. For example:
